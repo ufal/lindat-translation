d099a74b272d4cb3cbf87a75bfe9b14d6ea03cc9
kosarko <kosarko@ufal.mff.cuni.cz>
2018-02-08 16:30:48 +0000

    bind on all iface

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from uwsgi import app
 
 
 manager = Manager(app)
-manager.add_command('runserver', Server(port=5000, use_debugger=True, use_reloader=True))
+manager.add_command('runserver', Server(host='0.0.0.0', port=5000, use_debugger=True, use_reloader=True))
 
 
 @manager.command
