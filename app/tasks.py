281a969eae8dc1e649d55d37dc0b16575cb42cf1
kosarko <kosarko@ufal.mff.cuni.cz>
2018-02-09 09:49:05 +0000

    returns the translation to console

--- app/tasks.py
+++ app/tasks.py
@@ -6,15 +6,17 @@ from flask import current_app
 def run(english):
     if english:
         import tempfile
-        with tempfile.TemporaryFile(delete=False) as temp:
+        with tempfile.NamedTemporaryFile(mode='wt', encoding='utf-8', delete=False) as temp:
             temp.write(english)
-            return '{} '.format(temp.name)
-        #import time
-        #start_time = time.time()
-        #import subprocess
-        #subprocess.check_call(['/home/okosarko/run_transformer.sh', '/home/varis/test.in'])
-        #seconds = time.time() - start_time
-        ##seconds = random.randint(1, current_app.config['MAX_TIME_TO_WAIT'])
+            temp.flush()
+            import subprocess
+            subprocess.check_call(['/home/okosarko/run_transformer.sh', temp.name])
+            output_name = temp.name + '.transformer.transformer_big_single_gpu.translate_encs_wmt_czeng57m32k.beam4.alpha0.6.decodes'
+            with open(output_name, mode='rt', encoding='utf-8') as output:
+                czech = output.read()
+                return czech
+            #seconds = time.time() - start_time
+            #seconds = random.randint(1, current_app.config['MAX_TIME_TO_WAIT'])
     else:
         1 / 0
 
--- run_transformer.sh
+++ run_transformer.sh
@@ -18,3 +18,7 @@ tensor2tensor-1.2.9/tensor2tensor/bin/t2t-decoder \
 	--eval_steps=0 \
 	--decode_from_file=$INPUT \
         --decode_to_file=/tmp/$OUTPUT
+wait
+while [ ! -f "/tmp/${OUTPUT}.transformer.transformer_big_single_gpu.translate_encs_wmt_czeng57m32k.beam4.alpha0.6.decodes" ]; do
+   sleep 10
+done
