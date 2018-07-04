e7ddf69c7557de2e97d17f16701f0aeb6836491e
kosarko <kosarko@ufal.mff.cuni.cz>
2018-07-04 09:11:32 +0000

    updated run_trasnformer.sh

--- app/tasks.py
+++ app/tasks.py
@@ -6,13 +6,13 @@ from flask import current_app
 def run(english):
     if english:
         import tempfile
-        with tempfile.NamedTemporaryFile(mode='wt', encoding='utf-8', delete=False) as temp:
-            temp.write(english)
-            temp.flush()
+        with tempfile.NamedTemporaryFile(mode='wt', encoding='utf-8', delete=False) as temp_in:
+            temp_in.write(english)
+            temp_in.flush()
+            output_name = temp_in.name + '_out'
             import subprocess
-            subprocess.check_call(['/home/okosarko/run_transformer.sh', temp.name],
+            subprocess.check_call(['/home/okosarko/run_transformer.sh', temp_in.name, output_name],
                                   timeout=10800)  # 3h
-            output_name = temp.name + '.transformer.transformer_big_single_gpu.translate_encs_wmt_czeng57m32k.beam4.alpha0.6.decodes'
             with open(output_name, mode='rt', encoding='utf-8') as output:
                 czech = output.read()
                 return czech
--- run_transformer.sh
+++ run_transformer.sh
@@ -2,26 +2,24 @@
 echo "Started" > /tmp/debug.log
 #INPUT=/home/varis/test.in
 INPUT=$1
-OUTPUT=$(basename $INPUT)
+OUTPUT=$2
+LANG_PAIR=${3:-"en-cs"}
 cd /home/okosarko/scripts
 cp $INPUT ${INPUT}_1
-./split-sentences.pl < ${INPUT}_1 > $INPUT
+./split-sentences.pl -l ${LANG_PAIR%-*} < ${INPUT}_1 > $INPUT
 source /home/varis/tensorflow-virtualenv/bin/activate
-export PYTHONPATH=/home/varis/tensor2tensor-1.2.9/
+export PYTHONPATH=/home/varis/tensor2tensor-1.4.2/
 cd /home/varis/
-tensor2tensor-1.2.9/tensor2tensor/bin/t2t-decoder \
+tensor2tensor-1.4.2/tensor2tensor/bin/t2t-decoder \
 	--t2t_usr_dir=~varis/t2t_usr_dir/ \
 	--data_dir=~varis/t2t_data_dir/ \
 	--model=transformer \
 	--hparams_set=transformer_big_single_gpu \
-	--output_dir=~varis/t2t-model \
+	--output_dir=~varis/t2t-model.$LANG_PAIR \
 	--problems=translate_encs_wmt_czeng57m32k \
-	--decode_hparams=beam_size=4,alpha=0.6 \
+	--decode_hparams=beam_size=4,alpha=1.0 \
 	--train_steps=0 \
 	--eval_steps=0 \
 	--decode_from_file=$INPUT \
-        --decode_to_file=/tmp/$OUTPUT
+        --decode_to_file=$OUTPUT
 wait
-while [ ! -f "/tmp/${OUTPUT}.transformer.transformer_big_single_gpu.translate_encs_wmt_czeng57m32k.beam4.alpha0.6.decodes" ]; do
-   sleep 10
-done
