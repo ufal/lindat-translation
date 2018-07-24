5690c2fd4d0bead0c420e05d5a11af54eb1bf9ff
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2018-07-24 15:56:18 +0200

    add sentence splitter

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from .forms import TaskForm
 import tensorflow as tf
 from tensor2tensor.serving import serving_utils
 from tensor2tensor.utils import registry, usr_dir
+from sentence_splitter import split_text_into_sentences
 
 usr_dir.import_usr_dir('~varis/t2t_usr_dir')
 problem = registry.problem('translate_encs_wmt_czeng57m32k')
@@ -20,7 +21,7 @@ def run_task():
     lang_pair = request.form.get('lang_pair', default='en-cs')
     request_fn = serving_utils.make_grpc_request_fn(servable_name=lang_pair + '_model',
                                                                    server='localhost:9000', timeout_secs=90)
-    sentences = split_to_sent_array(text)
+    sentences = split_to_sent_array(text, lang=lang_pair.split('-')[0])
     outputs = list(map(lambda sent_score: sent_score[0],
                        serving_utils.predict(sentences, problem, request_fn)))
     return jsonify(outputs)
@@ -32,5 +33,5 @@ def index():
     return render_template('index.html', form=form)
 
 
-def split_to_sent_array(text):
-    return [text]
+def split_to_sent_array(text, lang):
+    return split_text_into_sentences(text=text, language=lang)
