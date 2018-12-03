d52d7a9fa13cf0248454afc47db3373e6010b7a1
kosarko <kosarko@ufal.mff.cuni.cz>
2018-12-03 15:34:41 +0000

    more sensible batching config

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,8 @@
 import os
 
 BOOTSTRAP_SERVE_LOCAL = True
-MAX_CONTENT_LENGTH = 32 * 1024
-BATCH_SIZE = 1000
+MAX_CONTENT_LENGTH = 100 * 1024
+BATCH_SIZE = 20 #1000
 #CSRF prevention
 SECRET_KEY = (os.environ.get('SECRET_KEY') or
               b'\x0c\x11{\xd3\x11$\xeeel\xa6\xfb\x1d~\xfd\xb3\x9d\x11\x00\xfb4\xd64\xd4\xe0')
