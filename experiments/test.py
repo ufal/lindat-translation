c0ef5cd2252f39d38979e831d5c3d87dc652d97a
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-09-10 17:09:48 +0200

    api

--- experiments/test.py
+++ experiments/test.py
@@ -16,3 +16,36 @@ with open("../file_samples/The-Oppidum-project.tmx", 'rb') as fin:
     for node in tmx_file.unit_iter():
         print(node.getsource(), node.gettarget())
 
+
+
+def file2segments(inputfile):
+    # test type of inputfile -- open if filename, stringstream if string,
+    # use directly if stream, otherwise error
+    inputstream = inputfile
+    pass
+    
+    # guess type of input -- try to load as tmx and as xliff, otherwise assume
+    # text
+    inputtype = 'TXT'
+    pass
+
+    # extract input segments
+    inputsegments = inputstream.readlines()    
+
+    # extract metadata if possible
+    srclang = None
+    tgtlang = None
+
+    # return inputsegments, inputtype, srclang, tgtlang
+    return (inputsegments, inputtype, srclang, tgtlang)
+
+translations2file(translationsegments, inputfile):
+    # again, detect input type and file type (share code)
+    pass
+
+    # insert translations
+    outputstring = translationsegments.join('\n')
+
+    # return file contents as string
+    return outputstring
+
