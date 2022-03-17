1a6cd503a6ff8f26f539f86102d0f96561f464da
Ondrej Kosarko <okosarko@translation-dev.servicescloud.ufal>
2022-03-17 15:16:01 +0000

    log ip
    this should help filter out consecutive inputs due to autotranslate

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE IF NOT EXISTS translations(src_lang TEXT, tgt_lang TEXT, src TEXT, tgt TEXT, author TEXT, frontend TEXT,
- inserted DATETIME default current_timestamp)
+ inserted DATETIME default current_timestamp);
+ALTER TABLE translations ADD ip_address TEXT default 'unknown';
