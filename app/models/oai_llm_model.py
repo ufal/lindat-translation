3b28019ee7ec05709ecf946ee3aff3be9d52ba81
cepin19 <jon@ufal.mff.cuni.cz>
2026-04-30 18:17:01 +0200

    async llm

--- app/models/oai_llm_model.py
+++ app/models/oai_llm_model.py
@@ -15,7 +15,7 @@ class OaiLLMModel(models.Model):
     def __init__(self, cfg):
         super().__init__(cfg)
         self.provider=cfg['provider']
-        default_prompt = "Translate the following text from {src} to {tgt}, including correctly transferring the markup from the source sentence into the translation. Do not add any explanations, make sure to only output the translated text, including markup like HTML tags, transferred from the source and nothing else. Do not exaplain any abrreviations or terms, only print out the translation, or, for untranslatable content, print out the source. Source text: {sentence} "
+        default_prompt = "Translate the following text from {src} to {tgt}, including correctly transferring the markup from the source sentence into the translation. Do not add any explanations, make sure to only output the translated text, including markup like HTML tags, transferred from the source and nothing else. Do not exaplain any abrreviations or terms, only print out the translation, or, for untranslatable content, print out the source. If there are not HTML tags in the source, do not add them. Source text: {sentence} "
         default_prompt_terms= "Translate the following text from {src} to {tgt}, including correctly transferring the markup from the source sentence into the translation. Do not add any explanations, make sure to only output one single line with the translated sentence and nothing else.  Use the following terminology database to translate specific terms: Source term -> Target term \n {terms} \n Source sentence: {sentence} "
         self.token=cfg.get('token', None)
         self.prompt=cfg.get('prompt', default_prompt)
